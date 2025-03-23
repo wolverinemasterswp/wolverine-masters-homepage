65dc35117274e5c0d7eb22fa888a1c2106d9d27b
Wolverine Masters Water Polo <wolverinemasterswp@gmail.com>
2025-03-23 12:43:03 -0400

    fix: base url routing for assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://wolverinemasterswp.github.io'
+  site: 'https://wolverinemasterswp.github.io',
+  base: '/wolverine-masters-homepage'
 })
